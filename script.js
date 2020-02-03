4c81d1716d7ff6101a24413d31ec2d38b01bb80b
Semih217706 <semihkasimoglu@gmail.com>
2020-02-03 14:24:37 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,65 @@
+const container = document.querySelector(".container");
+const seats = document.querySelectorAll(".row .seat:not(.occupied)");
+
+const movieSelect = document.getElementById("movie");
+// const ticketPrice = movieSelect.value; //string
+let ticketPrice = +movieSelect.value; //number
+
+let count = document.getElementById("count");
+let total = document.getElementById("total");
+
+populateUI();
+//selected movie
+
+function setMovieData(movieIndex, moviePrice) {
+  localStorage.setItem("selectedMovieIndex", movieIndex);
+  localStorage.setItem("selectedMoviePrice", moviePrice);
+}
+
+//
+function updateSelectedCount() {
+  const selectedSeats = document.querySelectorAll(".row .seat.selected");
+
+  const seatsIndex = [...selectedSeats].map(seat => {
+    return [...seats].indexOf(seat);
+  });
+  localStorage.setItem("seats", JSON.stringify(seatsIndex));
+
+  const selectedSeatsCount = selectedSeats.length;
+  //   console.log(selectedSeatsCount);
+  count.innerText = selectedSeatsCount;
+  total.innerText = selectedSeatsCount * ticketPrice;
+}
+
+container.addEventListener("click", e => {
+  if (
+    e.target.classList.contains("seat") &&
+    !e.target.classList.contains("occupied")
+  ) {
+    e.target.classList.toggle("selected");
+    updateSelectedCount();
+  }
+});
+
+function populateUI() {
+  const selectedSeats = JSON.parse(localStorage.getItem("selectedSeats"));
+  if (selectedSeats !== null && selectedSeats.length > 0) {
+    seats.forEach((seat, index) => {
+      if (selectedSeats.indexOf(seat) > -1) {
+        seat.classList.add("selected");
+      }
+    });
+  }
+  const selectedMovieIndex = localStorage.getItem("selectedMovieIndex");
+  if (selectedMovieIndex !== null) {
+    movieSelect.selectedIndex = selectedMovieIndex;
+  }
+}
+
+// movie select event
+movieSelect.addEventListener("change", e => {
+  ticketPrice = e.target.value;
+  console.log(e.target.selectedIndex);
+  setMovieData(e.target.selectedIndex, e.target.value);
+  updateSelectedCount();
+});
